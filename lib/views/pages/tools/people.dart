65e8e08713828754f9e3b893ef8e350d962b1429
Noel Pinto <noelpinto47@gmail.com>
2025-05-29 22:28:12 +0530

    added people page

--- lib/views/pages/tools/people.dart
+++ lib/views/pages/tools/people.dart
@@ -1,62 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:planora/utils/font_weights.dart';
 
 class People extends StatelessWidget {
   const People({super.key});
 
   static const templateNames = [
+    "Noel Pinto",
     "Hansel Presley Saldanha",
     "Eben Dsouza",
     "Umraan Mastan",
   ];
 
-  static const lastActiveDate = [
-    "19th May 2024",
-    "09th May 2024",
-    "23rd April 2024",
-  ];
-
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    const circlesPerRow = 3;
+    const spacing = 24.0;
+    final totalSpacing = (circlesPerRow - 1) * spacing;
+    final diameter = (screenWidth - totalSpacing) / circlesPerRow;
+    final radius = diameter / 2;
     return Scaffold(
       appBar: AppBar(title: Text("People")),
-      body: Center(
-        child: ListView.separated(
-          padding: EdgeInsets.only(left: 8.0, right: 8.0, top: 16.0),
-          separatorBuilder:
-              (context, index) => Divider(color: Colors.transparent),
-          itemCount: 3,
-          itemBuilder:
-              (context, index) => ListTile(
-                minTileHeight: 100.0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                tileColor: Theme.of(context).colorScheme.primary,
-                title: Text(
-                  templateNames[index],
-                  style: TextStyle(
-                    fontWeight: FontWeights.regular,
-                    color: Theme.of(context).colorScheme.onPrimary,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.only(top: 8.0, bottom: 8.0, left: 4.0, right: 4.0),
+        child: Wrap(
+          runSpacing: 24.0,
+          spacing: 16.0,
+          children: List.generate(
+            templateNames.length,
+            (index) => Column(
+              spacing: 8.0,
+              children: [
+                CircleAvatar(
+                  radius: radius,
+                  foregroundImage: NetworkImage(
+                    "https://randomuser.me/api/portraits/men/${index + 1}.jpg",
                   ),
                 ),
-                // subtitle: Text("Country: USA"),
-                trailing: IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.add,
-                    color: Theme.of(context).colorScheme.onPrimary,
-                  ),
+                Text(
+                  templateNames[index].split(" ").join("\n"),
+                  textAlign: TextAlign.center,
                 ),
-                subtitle: Text(
-                  "Last Active on: ${lastActiveDate[index]}",
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.onPrimary,
-                  ),
-                ),
-                visualDensity: VisualDensity.comfortable,
-                titleAlignment: ListTileTitleAlignment.center,
-              ),
+              ],
+            ),
+          ),
         ),
       ),
     );
