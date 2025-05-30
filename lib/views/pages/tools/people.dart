434fc91a1aadddfe9f8d710ad89aeed308e44f65
Noel Pinto <noelpinto47@gmail.com>
2025-05-30 11:58:17 +0530

    added people list

--- lib/views/pages/tools/people.dart
+++ lib/views/pages/tools/people.dart
@@ -1,8 +1,23 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:planora/views/pages/tools/people/contact_list.dart';
+import 'package:planora/views/pages/tools/people/create_new_contact.dart';
 
-class People extends StatelessWidget {
+class People extends StatefulWidget {
   const People({super.key});
 
+  @override
+  State<People> createState() => _PeopleState();
+}
+
+class _PeopleState extends State<People> {
+  static const contacts = [
+    {"name": "Noel Pinto", "phoneNumber": "1234567890"},
+    {"name": "Hansel Presley Saldanha", "phoneNumber": "1234567890"},
+    {"name": "Eben Dsouza", "phoneNumber": "1234567890"},
+    {"name": "Umraan Mastan", "phoneNumber": "1234567890"},
+  ];
   static const templateNames = [
     "Noel Pinto",
     "Hansel Presley Saldanha",
@@ -44,6 +59,182 @@ class People extends StatelessWidget {
             ),
           ),
         ),
+      ),
+      floatingActionButton: SpeedDial(
+        icon: Icons.add,
+        activeIcon: Icons.close,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        foregroundColor: Theme.of(context).colorScheme.onPrimary,
+        overlayOpacity: 0.4,
+        spacing: 12,
+        spaceBetweenChildren: 12,
+        childrenButtonSize: const Size(56, 56),
+        shape: const CircleBorder(),
+        children: [
+          SpeedDialChild(
+            shape: const CircleBorder(),
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            foregroundColor: Theme.of(context).colorScheme.onPrimary,
+            child: Icon(Icons.contact_page),
+            label: 'Add From Contacts',
+            labelBackgroundColor: Colors.transparent,
+            labelShadow: List.empty(),
+            onTap: () {
+              showCupertinoModalPopup<void>(
+                context: context,
+                builder:
+                    (context) => CupertinoContextMenu.builder(
+                      actions: [
+                        CupertinoContextMenuAction(
+                          child: Text("Add From Contacts"),
+                          onPressed: () {},
+                        ),
+                      ],
+                      builder: (
+                        BuildContext context,
+                        Animation<double> animation,
+                      ) {
+                        return Container(
+                          height: MediaQuery.of(context).size.height * 0.6,
+                          alignment: Alignment.topCenter,
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).colorScheme.primary,
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          width: double.infinity,
+                          child: MediaQuery.removePadding(
+                            context: context,
+                            removeTop: true,
+                            child:
+                                Theme.of(context).platform == TargetPlatform.iOS
+                                    ? CupertinoPageScaffold(
+                                      backgroundColor:
+                                          Theme.of(context).colorScheme.surface,
+                                      navigationBar: CupertinoNavigationBar(
+                                        automaticallyImplyLeading: false,
+                                        middle: Text(
+                                          "Contact List",
+                                          style: TextStyle(
+                                            color:
+                                                Theme.of(
+                                                  context,
+                                                ).colorScheme.onSurface,
+                                          ),
+                                        ),
+                                        trailing: CupertinoButton(
+                                          sizeStyle: CupertinoButtonSize.medium,
+                                          child: Text(
+                                            "Close",
+                                            style: TextStyle(
+                                              color:
+                                                  Theme.of(
+                                                    context,
+                                                  ).colorScheme.primary,
+                                            ),
+                                          ),
+                                          onPressed: () {
+                                            Navigator.pop(context);
+                                          },
+                                        ),
+                                      ),
+                                      child: ListView.builder(
+                                        itemCount: contacts.length,
+                                        itemBuilder: (context, index) {
+                                          final contact = contacts[index];
+                                          return CupertinoListSection(
+                                            backgroundColor:
+                                                Theme.of(
+                                                  context,
+                                                ).colorScheme.surface,
+                                            header: Text(
+                                              contact["name"]!.substring(0, 1),
+                                            ),
+                                            children: [
+                                              CupertinoListTile(
+                                                leading: CircleAvatar(
+                                                  child: Text(
+                                                    contact["name"]!.substring(
+                                                      0,
+                                                      1,
+                                                    ),
+                                                  ),
+                                                ),
+                                                title: Text(
+                                                  contact["name"]!,
+                                                  style: TextStyle(
+                                                    color:
+                                                        Theme.of(
+                                                          context,
+                                                        ).colorScheme.onSurface,
+                                                  ),
+                                                ),
+                                              ),
+                                            ],
+                                          );
+                                        },
+                                      ),
+                                    )
+                                    : Scaffold(
+                                      appBar: AppBar(
+                                        title: Text("Contact List"),
+                                        automaticallyImplyLeading: false,
+                                        actions: [
+                                          IconButton(
+                                            icon: Icon(Icons.close),
+                                            onPressed: () {
+                                              Navigator.pop(context);
+                                            },
+                                          ),
+                                        ],
+                                      ),
+                                      body: ListView.builder(
+                                        itemCount: contacts.length,
+                                        itemBuilder: (context, index) {
+                                          final contact = contacts[index];
+                                          return ListTile(
+                                            leading: CircleAvatar(
+                                              child: Text(
+                                                contact["name"]!.substring(
+                                                  0,
+                                                  1,
+                                                ),
+                                              ),
+                                            ),
+                                            title: Text(contact["name"]!),
+                                            subtitle: Text(
+                                              contact["phoneNumber"]!,
+                                            ),
+                                            trailing: Icon(Icons.arrow_forward),
+                                            onTap: () {},
+                                          );
+                                        },
+                                      ),
+                                    ),
+                          ),
+                        );
+                      },
+                    ),
+              );
+            },
+          ),
+          SpeedDialChild(
+            shape: const CircleBorder(),
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            foregroundColor: Theme.of(context).colorScheme.onPrimary,
+            child: Icon(Icons.create),
+            label: 'Create New',
+            labelBackgroundColor: Colors.transparent,
+            labelShadow: List.empty(),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const CreateNewContact(),
+                ),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
