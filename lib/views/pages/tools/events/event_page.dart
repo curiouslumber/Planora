34aef39ad39ea9c1ac7b93ba01b99b9c2ca301e2
Noel Pinto <noelpinto47@gmail.com>
2025-06-02 23:48:34 +0530

    updated

--- lib/views/pages/tools/events/event_page.dart
+++ lib/views/pages/tools/events/event_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:planora/models/event_model.dart';
 
 class EventPage extends StatefulWidget {
-  const EventPage({super.key});
+  const EventPage({super.key, required EventModel event});
 
   @override
   State<EventPage> createState() => _EventPageState();
