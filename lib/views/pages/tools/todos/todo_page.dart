da07536b94c25b7d9bbb3ec3270903546b114667
Noel Pinto <noelpinto47@gmail.com>
2025-07-25 18:04:35 +0530

    updated

--- lib/views/pages/tools/todos/todo_page.dart
+++ lib/views/pages/tools/todos/todo_page.dart
@@ -49,7 +49,10 @@ class _TodoPageState extends State<TodoPage> {
           setState(() {
             _todo = _todo.copyWith(
               todo: _todo.todo,
-              todoStatus: _todo.todoStatus == Constants.todoStatus[1] ? Constants.todoStatus[0] : Constants.todoStatus[1],
+              todoStatus:
+                  _todo.todoStatus == Constants.todoStatus[1]
+                      ? Constants.todoStatus[0]
+                      : Constants.todoStatus[1],
               updatedAt: DateTime.now(),
               createdAt: _todo.createdAt,
               priority: _todo.priority,
@@ -61,7 +64,13 @@ class _TodoPageState extends State<TodoPage> {
             FirebaseFirestoreService().updateTodoDocument(_todo.id, _todo);
           });
         },
-        label: Text(isDone ? 'Mark as Undone' : 'Mark as Done', style: Theme.of(context).textTheme.titleMedium?.copyWith(fontWeight: FontWeight.w600)),
+        label: Text(
+          isDone ? 'Mark as Undone' : 'Mark as Done',
+          style: Theme.of(context).textTheme.titleMedium?.copyWith(
+            fontWeight: FontWeight.w600,
+            color: Theme.of(context).colorScheme.onPrimary,
+          ),
+        ),
         backgroundColor: Theme.of(context).colorScheme.primary,
       ),
       body: ListView(
@@ -69,40 +78,74 @@ class _TodoPageState extends State<TodoPage> {
         children: [
           ListTile(
             leading: Icon(Icons.flag),
-            title: Text('Priority', style: Theme.of(context).textTheme.titleMedium?.copyWith(fontWeight: FontWeight.w600)),
+            title: Text(
+              'Priority',
+              style: Theme.of(
+                context,
+              ).textTheme.titleMedium?.copyWith(fontWeight: FontWeight.w600),
+            ),
             trailing: Chip(
               label: Text(_todo.priority),
-              backgroundColor: _todo.priority == Constants.priority[2]
-                  ? Colors.redAccent
-                  : _todo.priority == Constants.priority[1]
+              backgroundColor:
+                  _todo.priority == Constants.priority[2]
+                      ? Colors.redAccent
+                      : _todo.priority == Constants.priority[1]
                       ? Colors.orangeAccent
                       : _todo.priority == Constants.priority[0]
-                          ? Colors.green
-                          : Colors.grey,
+                      ? Colors.green
+                      : Colors.grey,
               labelStyle: TextStyle(color: Colors.white),
             ),
           ),
           ListTile(
             leading: Icon(Icons.repeat),
-            title: Text('Repeat', style: Theme.of(context).textTheme.titleMedium?.copyWith(fontWeight: FontWeight.w600)),
-            subtitle: Text(_todo.repeatOption == Constants.repeatOptions[0]
-                ? 'Does not repeat'
-                : '${_todo.repeatOption} (${_todo.selectedDays.join(", ")})'),
+            title: Text(
+              'Repeat',
+              style: Theme.of(
+                context,
+              ).textTheme.titleMedium?.copyWith(fontWeight: FontWeight.w600),
+            ),
+            subtitle: Text(
+              _todo.repeatOption == Constants.repeatOptions[0]
+                  ? 'Does not repeat'
+                  : '${_todo.repeatOption} (${_todo.selectedDays.join(", ")})',
+            ),
           ),
           ListTile(
             leading: Icon(Icons.calendar_today),
-            title: Text('Created', style: Theme.of(context).textTheme.titleMedium?.copyWith(fontWeight: FontWeight.w600)),
+            title: Text(
+              'Created',
+              style: Theme.of(
+                context,
+              ).textTheme.titleMedium?.copyWith(fontWeight: FontWeight.w600),
+            ),
             subtitle: Text(DateFormat.yMMMd().add_jm().format(_todo.createdAt)),
           ),
           ListTile(
             leading: Icon(Icons.update),
-            title: Text('Last Updated', style: Theme.of(context).textTheme.titleMedium?.copyWith(fontWeight: FontWeight.w600)),
+            title: Text(
+              'Last Updated',
+              style: Theme.of(
+                context,
+              ).textTheme.titleMedium?.copyWith(fontWeight: FontWeight.w600),
+            ),
             subtitle: Text(DateFormat.yMMMd().add_jm().format(_todo.updatedAt)),
           ),
           ListTile(
-            leading: Icon(isDone ? Icons.check_circle : Icons.radio_button_unchecked),
-            title: Text('Status', style: Theme.of(context).textTheme.titleMedium?.copyWith(fontWeight: FontWeight.w600)),
-            subtitle: Text(_todo.todoStatus == Constants.todoStatus[1] ? Constants.todoStatus[1] : Constants.todoStatus[0]),
+            leading: Icon(
+              isDone ? Icons.check_circle : Icons.radio_button_unchecked,
+            ),
+            title: Text(
+              'Status',
+              style: Theme.of(
+                context,
+              ).textTheme.titleMedium?.copyWith(fontWeight: FontWeight.w600),
+            ),
+            subtitle: Text(
+              _todo.todoStatus == Constants.todoStatus[1]
+                  ? Constants.todoStatus[1]
+                  : Constants.todoStatus[0],
+            ),
           ),
         ],
       ),
